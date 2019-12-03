9ac327833e2895a8527e283dc293cd6d0750b375
Sardor <xst3427472@gmail.com>
2019-12-03 18:06:59 +0500

    pushed from master

--- app/js/main.js
+++ app/js/main.js
@@ -6,18 +6,23 @@ $(document).ready(function () {
         loop: true,
     });
 
-    $('.project-main-slider').owlCarousel({
-        items: 1,
-        nav: false,
-        dots: true,
-    })
 
-
-    $('.gallery-slider').owlCarousel({
-        items: 1,
-        nav: false,
-        dots: true,
-    })
+    // $('.gallery-slider').owlCarousel({
+    //     margin: 15,
+    //     nav: false,
+    //     dots: true,
+    //     responsive: {
+    //         0: {
+    //             items: 1
+    //         },
+    //         768: {
+    //             items: 3
+    //         },
+    //         992: {
+    //             items: 4
+    //         }
+    //     }
+    // });
 
     $('.partnior-img').owlCarousel({
         responsive: {
@@ -40,10 +45,88 @@ $(document).ready(function () {
                 items: 1
             }
         }
-    })
+    });
+
+    open();
+
+    var btn = $('.btn');
+
+    btn.on('click', function () {
+        $(this).toggleClass('active');
+        $(this).toggleClass('not-active');
+    });
+
+    // main slider
+    if (windowWidth > 991) {
+        mainSlider(3)
+        gallerySlider(4)
+    } else if (windowWidth > 768) {
+        mainSlider(2)
+        gallerySlider(2)
+    } else {
+        mainSlider(1)
+        gallerySlider(2)
+    }//  main slider
+
+
+
+
+
 
 
 });
+
+function open() {
+    $('.hamburger').click(function () {
+        const nav = $('#list').toggleClass('open');
+    });
+}
+
+// render main slider function
+const buildProjectMainSlider = function (selector, sliderInner) {
+    const projectMainSlider = selector;
+    const projectMainSliderElement = projectMainSlider.find(sliderInner);
+    return function (n) {
+        var projectMainSliders = '<div>';
+        var i = 1;
+        projectMainSliderElement.each(function (index) {
+
+            projectMainSliders += $(this).prop('outerHTML');
+            if (!(index + 1 === projectMainSliderElement.length)) {
+                if (i % n === 0) {
+                    projectMainSliders += '</div><div>';
+                }
+                i++;
+            }
+        });
+        projectMainSliders += '</div>';
+        projectMainSlider.html(projectMainSliders);
+        projectMainSlider.owlCarousel({
+            nav: false,
+            dots: true,
+            margin: 15,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                768: {
+                    items: 2
+                },
+                992: {
+                    items: 3
+                }
+            }
+        });
+    }
+};
+const mainSlider = buildProjectMainSlider($('.project-main-slider'), '.project-main-element-card');
+const windowWidth = $(window).outerWidth();
+const gallerySlider = buildProjectMainSlider($('.gallery-slider'), '.gallery-slider-card');
+
+
+
+
+
 
 
 
